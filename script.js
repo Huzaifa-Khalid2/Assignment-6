ce3446923d9ce823c672f5924c8a800fb8886c73
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-21 14:57:02 +0000

    fixed everything

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 document.getElementById('my-btn').addEventListener('click', myFunction)
 function myFunction () {
-   let pizzasize = ''
-   let topping = ''
-   pizzasize = document.getElementById('my-text').value
-   topping = document.getElementById('my-text2').value
-  if ((pizzasize == 'large') && (topping == 'one')) {
+  let pizzasize = ''
+  let topping = ''
+  pizzasize = document.getElementById('my-text').value
+  topping = document.getElementById('my-text2').value
+  if ((pizzasize === 'large') && (topping === 'one')) {
     alert('$6.00 + $1 x 1.13 so you must pay $7.91')
-  } else if ((pizzasize == 'large') && (topping == 'two')) {
+  } else if ((pizzasize === 'large') && (topping === 'two')) {
     alert('$6.00 + 1.75 x 1.13 so you must pay $8.76')
-  } else if ((pizzasize == 'large') && (topping == 'three')) {
+  } else if ((pizzasize === 'large') && (topping === 'three')) {
     alert('$6.00 + $2.50 x 1.13 so you must pay $9.61')
-  } else if ((pizzasize == 'large') && (topping == 'four')) {
+  } else if ((pizzasize === 'large') && (topping === 'four')) {
     alert('$6.00 + $3.35 x 1.13 so you must pay $10.57')
-  } else if ((pizzasize == 'Extralarge') && (topping == 'one')) {
+  } else if ((pizzasize === 'Extralarge') && (topping === 'one')) {
     alert('$10.00 + $1 x 1.13 so you must pay $12.43')
-  } else if ((pizzasize == 'Extralarge') && (topping == 'two')) {
+  } else if ((pizzasize === 'Extralarge') && (topping === 'two')) {
     alert('$10.00 + 1.75 x 1.13 so you must pay $13.28')
-  } else if ((pizzasize == 'Extralarge') && (topping == 'three')) {
+  } else if ((pizzasize === 'Extralarge') && (topping === 'three')) {
     alert('$10.00 + $2.50 x 1.13 so you must pay $14.13')
-  } else if ((pizzasize == 'Extralarge') && (topping == 'four')) {
+  } else if ((pizzasize === 'Extralarge') && (topping === 'four')) {
     alert('$10.00 + $3.35 x 1.13 so you must pay $15.09')
   } else {
     alert('Undifined please enter both toppings and pizza size')
